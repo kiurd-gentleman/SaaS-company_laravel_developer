a6cc1e962dad2c194c42ef608eaccbdb2722cf1d
kiurd-gentelman <coredeveloper.2013@gmail.com>
2024-04-03 23:11:17 +0600

    login and register blade design change

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,19 +1,8 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
-
-                <div class="card-body">
-                    <register-component></register-component>
-
-                </div>
-            </div>
-        </div>
-    </div>
+<div>
+    <register-component></register-component>
 </div>
 @endsection
 <script>
